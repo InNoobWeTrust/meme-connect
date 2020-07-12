f9b5df5254df6ae9d49d2380a9ce33feb55c0231
InNoobWeTrust <forbidden.gun@gmail.com>
2020-07-12 06:10:19 -0700

    Implement shadow casting logic
    - Improve structure and functions to get set game map.
    - Implement vertical and horizontal shadow tracing for game map.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 extern crate rand;
 
 use rand::prelude::*;
+use std::{
+    iter::{Skip, StepBy, Take},
+    slice::Iter,
+};
 
 pub type Meme = usize;
 const NO_MEME: Meme = 0;
@@ -12,132 +16,223 @@ pub struct ShadowTrace {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
-pub struct Point {
+pub struct Block {
     pub x: usize,
     pub y: usize,
 }
 
-impl Point {
-    pub fn rand(rng: &mut ThreadRng, from: &mut Vec<Point>) -> Point {
-        from.remove(rng.gen_range(0, from.len()))
+impl Block {
+    pub fn new(x: usize, y: usize) -> Block {
+        Block { x, y }
     }
 }
 
 pub struct GameMap {
-    data: Vec<Vec<Meme>>,
+    pub width: usize,
+    pub height: usize,
+    data: Vec<Meme>,
 }
 
 impl GameMap {
-    pub fn new(width: usize, height: usize) -> GameMap {
-        GameMap {
-            data: vec![vec![NO_MEME; width]; height],
+    pub fn new(width: usize, height: usize) -> Option<GameMap> {
+        if 0 != width && 0 != height && 0 == (width * height) % 2 {
+            Some(GameMap {
+                width,
+                height,
+                data: vec![NO_MEME; width * height],
+            })
+        } else {
+            None
         }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    ////////////////////////////// View of data ///////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    fn col<'a>(&'a self, index: usize) -> StepBy<Skip<Iter<'a, usize>>> {
+        self.data.iter().skip(index).step_by(self.width)
+    }
+
+    fn row<'a>(&'a self, index: usize) -> Take<Skip<Iter<'a, usize>>> {
+        self.data.iter().skip(index * self.height).take(self.height)
+    }
+
+    fn rows<'a>(&'a self) -> Vec<Take<Skip<Iter<'a, usize>>>> {
+        (0..self.height)
+            .into_iter()
+            .map(|y| self.row(y))
+            .collect::<Vec<_>>()
+    }
+
+    fn cols<'a>(&'a self) -> Vec<StepBy<Skip<Iter<'a, usize>>>> {
+        (0..self.width)
+            .into_iter()
+            .map(|x| self.col(x))
+            .collect::<Vec<_>>()
+    }
+
+    pub fn playground_blocks(&self) -> Vec<Block> {
+        (1..self.width - 1)
+            .into_iter()
+            .flat_map(move |x| {
+                (1..self.height - 1)
+                    .into_iter()
+                    .map(move |y| Block::new(x, y))
+            })
+            .collect::<Vec<_>>()
     }
 
     pub fn _fmt(&self) -> String {
-        self.data
-            .iter()
+        self.rows()
+            .iter_mut()
             .map(|line| {
                 format!(
                     "|{}|",
-                    line.iter()
-                        .map(|meme| format!("{:^5}", meme))
-                        .collect::<Vec<String>>()
+                    line.map(|meme| format!("{:^5}", meme))
+                        .collect::<Vec<_>>()
                         .join("|")
                 )
             })
-            .collect::<Vec<String>>()
+            .collect::<Vec<_>>()
             .join("\n")
     }
 
-    pub fn check_border_block(&self, pos: Point) -> Result<(), String> {
-        if pos.x == 0 {
-            return Err("border left".to_string());
+    ///////////////////////////////////////////////////////////////////////////
+    ////////////////////////// Position validation ////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    fn is_idx_border(&self, idx: usize) -> bool {
+        // Top border
+        idx < self.width ||
+            // Bottom border
+            idx > self.width * self.height - 1 ||
+            // Left border
+            idx % self.width == 0 ||
+            // Right border
+            idx % self.width == self.width - 1
+    }
+
+    fn block2idx(&self, blk: &Block) -> usize {
+        blk.y * self.width + blk.x
+    }
+
+    fn check_border_block(&self, blk: &Block) -> Result<(), String> {
+        if self.is_idx_border(self.block2idx(blk)) {
+            Err("border".to_string())
+        } else {
+            Ok(())
         }
-        if pos.x == self.data[0].len() - 1 {
-            return Err("border right".to_string());
+    }
+
+    pub fn check_valid_empty_block(&self, blk: &Block) -> Result<(), String> {
+        if let Err(err) = self.check_border_block(blk) {
+            return Err(format!("{:?} is at {}", blk, err));
         }
-        if pos.y == 0 {
-            return Err("border top".to_string());
+        match self.data[self.block2idx(blk)] {
+            NO_MEME => Ok(()),
+            meme => Err(format!("{:?} occupied with {}", blk, meme)),
         }
-        if pos.y == self.data.len() - 1 {
-            return Err("border bottom".to_string());
-        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    //////////////////////// Helpers to get set meme //////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    pub fn collect_empty_blocks<'a>(
+        &self,
+        from: &mut dyn Iterator<Item = &'a Block>,
+    ) -> Vec<&'a Block> {
+        from.filter(|blk| self.check_valid_empty_block(blk).is_ok())
+            .collect::<Vec<_>>()
+    }
+
+    pub fn set_meme(&mut self, meme: Meme, blk: &Block) -> Result<(), String> {
+        self.check_valid_empty_block(blk)?;
+        let idx = self.block2idx(blk);
+        self.data[idx] = meme;
         Ok(())
     }
 
-    pub fn collect_empty_blocks(&self) -> Vec<Point> {
-        self.data
-            .iter()
-            .enumerate()
-            .flat_map(|(y, line)| {
-                line.iter()
-                    .enumerate()
-                    // Only take empty block
-                    .filter(|(_x, &val)| NO_MEME == val)
-                    .map(move |(x, &_val)| Point { x, y })
-            })
-            .filter(|&block| {
-                if let Ok(()) = self.check_border_block(block) {
-                    true
-                } else {
-                    false
+    pub fn set_meme_regions(
+        &mut self,
+        meme_lst: &mut dyn Iterator<Item = Meme>,
+        regions: &mut Vec<&Block>,
+        rng: &mut ThreadRng,
+    ) -> Result<(), String> {
+        regions.shuffle(rng);
+        for meme in meme_lst {
+            if 2 > regions.len() {
+                break;
+            }
+            for _ in 0..2 {
+                let blk = regions.pop().unwrap();
+                if let Err(err) = self.set_meme(meme, blk) {
+                    return Err(format!("Cannot set meme at {:?}: {}", blk, err));
                 }
-            })
-            .collect::<Vec<Point>>()
-    }
-
-    pub fn check_valid_empty_block(&self, pos: Point) -> Result<(), String> {
-        if let Err(err) = self.check_border_block(pos) {
-            return Err(format!("{:?} is at {}", pos, err));
-        }
-        match self.data[pos.y][pos.x] {
-            NO_MEME => Ok(()),
-            meme => Err(format!("{:?} occupied with {}", pos, meme)),
-        }
-    }
-
-    pub fn set_couple(&mut self, meme: Meme, pos1: Point, pos2: Point) -> Result<(), String> {
-        for &pos in [pos1, pos2].iter() {
-            if let Err(err) = self.check_valid_empty_block(pos) {
-                return Err(err);
             }
         }
-        self.data[pos1.y][pos1.x] = meme;
-        self.data[pos2.y][pos2.x] = meme;
         Ok(())
     }
-}
 
-pub fn shadow_tracing<'a, TrackIterable>(
-    on_track: &mut TrackIterable,
-    shadow_position: usize,
-    look_up_to_postion: usize,
-) -> Option<ShadowTrace>
-where
-    TrackIterable: IntoIterator<Item = &'a Meme>
-        + DoubleEndedIterator<Item = &'a Meme>
-        + ExactSizeIterator<Item = &'a Meme>,
-{
-    if let Some((pos_on_track, &meme)) = if shadow_position <= look_up_to_postion {
-        on_track
-            .enumerate()
-            .skip(shadow_position)
-            .take(look_up_to_postion - shadow_position + 1)
-            .inspect(|&(pos, &meme)| println!("=> tracing step: pos={}, meme={:?}", pos, meme))
-            .find(|&(_pos, &meme)| meme != NO_MEME)
-    } else {
-        on_track
-            .enumerate()
-            .skip(look_up_to_postion)
-            .rev()
-            .skip_while(|&(pos, &_meme)| pos > shadow_position)
-            .inspect(|&(pos, &meme)| println!("=> tracing step: pos={}, meme={:?}", pos, meme))
-            .find(|&(_pos, &meme)| meme != NO_MEME)
-    } {
-        return Some(ShadowTrace { meme, pos_on_track });
+    ///////////////////////////////////////////////////////////////////////////
+    //////////////////////////// Matching logic ///////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    pub fn shadow_tracing<'a, T>(
+        on_track: &mut T,
+        shadow_position: usize,
+        look_up_to_postion: usize,
+    ) -> Option<ShadowTrace>
+    where
+        T: DoubleEndedIterator<Item = &'a Meme> + ExactSizeIterator<Item = &'a Meme>,
+    {
+        if let Some((pos_on_track, &meme)) = if shadow_position <= look_up_to_postion {
+            on_track
+                .enumerate()
+                .skip(shadow_position)
+                .take(look_up_to_postion - shadow_position + 1)
+                .inspect(|&(pos, &meme)| println!("=> tracing step: pos={}, meme={:?}", pos, meme))
+                .find(|&(_pos, &meme)| meme != NO_MEME)
+        } else {
+            on_track
+                .enumerate()
+                .skip(look_up_to_postion)
+                .rev()
+                .skip_while(|&(pos, &_meme)| pos > shadow_position)
+                .inspect(|&(pos, &meme)| println!("=> tracing step: pos={}, meme={:?}", pos, meme))
+                .find(|&(_pos, &meme)| meme != NO_MEME)
+        } {
+            return Some(ShadowTrace { meme, pos_on_track });
+        }
+        None
     }
-    None
+
+    pub fn cast_horizontal_shadows<'a>(
+        &'a self,
+        horizontal_wall_idx: usize,
+        look_up_to_horizontal_idx: usize,
+    ) -> Vec<Option<ShadowTrace>> {
+        self.cols()
+            .into_iter()
+            .map(|mut col| {
+                GameMap::shadow_tracing(&mut col, horizontal_wall_idx, look_up_to_horizontal_idx)
+            })
+            .collect::<_>()
+    }
+
+    pub fn cast_vertical_shadows<'a>(
+        &'a self,
+        vertical_wall_idx: usize,
+        look_up_to_vertical_idx: usize,
+    ) -> Vec<Option<ShadowTrace>> {
+        self.rows()
+            .into_iter()
+            .map(|mut row| {
+                GameMap::shadow_tracing(&mut row, vertical_wall_idx, look_up_to_vertical_idx)
+            })
+            .collect::<_>()
+    }
 }
 
 #[cfg(test)]
@@ -161,10 +256,7 @@ mod test_tracing_shadow {
             look_up_to = rng.gen_range(0, TRACK.len() - 1);
         }
         println!("Wall position: {}, look up position: {}", wall, look_up_to);
-        match shadow_tracing(&mut TRACK.iter(), wall, look_up_to) {
-            Some(shadow) => println!("{:#?}", shadow),
-            None => println!("Cannot find subject in range that can cast shadow!"),
-        }
+        GameMap::shadow_tracing(&mut TRACK.iter(), wall, look_up_to).unwrap();
     }
 
     #[test]
@@ -177,7 +269,7 @@ mod test_tracing_shadow {
         }
         let look_up_to = rng.gen_range(0, TRACK.len() - 1);
         println!("Wall position: {}, look up position: {}", wall, look_up_to);
-        let shadow = shadow_tracing(&mut TRACK.iter(), wall, look_up_to).unwrap();
+        let shadow = GameMap::shadow_tracing(&mut TRACK.iter(), wall, look_up_to).unwrap();
         println!("{:#?}", shadow);
         assert_eq!(shadow.pos_on_track, wall);
         assert_eq!(shadow.meme, TRACK[wall]);
@@ -187,6 +279,6 @@ mod test_tracing_shadow {
     fn test_wall_meets_subject() {
         println!("Track: {:?}", TRACK);
         println!("Wall position: 1, look up position: 1");
-        shadow_tracing(&mut TRACK.iter(), 1, 1).unwrap();
+        GameMap::shadow_tracing(&mut TRACK.iter(), 1, 1).unwrap();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,24 @@ use meme_connect::*;
 
 fn main() {
     let mut rng = thread_rng();
-    let mut game_map = GameMap::new(12, 12);
+    let mut game_map = GameMap::new(10, 10).unwrap();
     println!("Game map:\n{}", game_map._fmt());
-    let mut empty_points = game_map.collect_empty_blocks();
-    for meme in 1.. {
-        if empty_points.is_empty() {
-            break;
-        }
-        while let Err(err) = game_map.set_couple(
-            meme,
-            Point::rand(&mut rng, &mut empty_points),
-            Point::rand(&mut rng, &mut empty_points),
-        ) {
-            println!("Trying again due to error: {}", err);
-        }
+    let playground = game_map.playground_blocks();
+    let empty_blocks = game_map.collect_empty_blocks(&mut playground.iter());
+    let mut interest_blocks = empty_blocks
+        .iter()
+        .filter(|blk| !(4..6).contains(&blk.x))
+        .filter(|blk| !(4..6).contains(&blk.y))
+        .map(|&blk| blk)
+        .collect::<Vec<&Block>>();
+    if let Err(err) =
+        game_map.set_meme_regions(&mut (1usize..).into_iter(), &mut interest_blocks, &mut rng)
+    {
+        panic!("Failed to set meme in regions: {}", err);
     }
     println!("Game map after filling some couples:\n{}", game_map._fmt());
-    const TRACK: &[Meme] = &[1, 2, 0, 3, 0, 0, 4, 0, 0, 5];
-    println!("Track: {:?}", TRACK);
-    let wall = rng.gen_range(0, TRACK.len() - 1);
-    let look_up_to = rng.gen_range(0, TRACK.len() - 1);
-    println!("Wall position: {}, look up position: {}", wall, look_up_to);
-    match shadow_tracing(&mut TRACK.iter(), wall, look_up_to) {
-        Some(shadow) => println!("{:#?}", shadow),
-        None => println!("Cannot find subject in range that can cast shadow!"),
-    }
+    let vertical_shadows = game_map.cast_vertical_shadows(4, game_map.width);
+    println!("Vertical shadows:\n{:#?}", vertical_shadows);
+    let horizontal_shadows = game_map.cast_horizontal_shadows(4, game_map.height);
+    println!("Horizontal shadows:\n{:#?}", horizontal_shadows);
 }
